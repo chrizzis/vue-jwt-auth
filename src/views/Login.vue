c5b1e557805492caedfc4c6875c42164238c86a3
chrizzis <christian.d.seabrook@gmail.com>
2021-10-10 11:05:50 -0700

    stub login n register forms

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,107 @@
 <template>
-  <div class="login">
-    <h1>login</h1>
-  </div>
+  <v-container>
+    <form>
+      <v-text-field
+        outlined
+        v-model="name"
+        :error-messages="nameErrors"
+        :counter="10"
+        label="Name"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-text-field
+        outlined
+        v-model="password"
+        :error-messages="passwordErrors"
+        label="Password"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-checkbox
+        v-model="checkbox"
+        label="Keep me logged in forever"
+      ></v-checkbox>
+
+      <v-btn class="mr-4" @click="submit"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </form>
+  </v-container>
 </template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, maxLength } from "vuelidate/lib/validators";
+
+export default {
+  mixins: [validationMixin],
+
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    password: {
+      required,
+      // minLength: minLength(8)  // I assume you'd want something like this too
+      containsUppercase: function (value) {
+        return /[A-Z]/.test(value);
+      },
+      containsLowercase: function (value) {
+        return /[a-z]/.test(value);
+      },
+      containsNumber: function (value) {
+        return /[0-9]/.test(value);
+      },
+      containsSpecial: function (value) {
+        return /[#?!@$%^&*-]/.test(value);
+      },
+    },
+  },
+
+  data: () => ({
+    name: "",
+    password: "",
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false,
+  }),
+
+  computed: {
+    nameErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      !this.$v.name.maxLength &&
+        errors.push("Name must be at most 10 characters long");
+      !this.$v.name.required && errors.push("Name is required.");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.required && errors.push("Password is required");
+      !this.$v.password.containsUppercase &&
+        errors.push("Password must contain an uppercase");
+      !this.$v.password.containsLowercase &&
+        errors.push("Password must contain an lowercase");
+      !this.$v.password.containsNumber &&
+        errors.push("Password must contain an number");
+      !this.$v.password.containsSpecial &&
+        errors.push("Password must contain an special");
+      return errors;
+    },
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.password = "";
+      this.select = null;
+      this.checkbox = false;
+    },
+  },
+};
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,157 @@
 <template>
-  <div class="register">
-    <h1>register</h1>
-  </div>
+  <v-container>
+    <form>
+      <v-text-field
+        outlined
+        v-model="name"
+        :error-messages="nameErrors"
+        :counter="10"
+        label="Name"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-text-field
+        outlined
+        v-model="email"
+        :error-messages="emailErrors"
+        label="E-mail"
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+      <v-text-field
+        outlined
+        v-model="password"
+        :error-messages="passwordErrors"
+        label="Password"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-select
+        v-model="select"
+        :items="items"
+        :error-messages="selectErrors"
+        label="Item"
+        required
+        @change="$v.select.$touch()"
+        @blur="$v.select.$touch()"
+      ></v-select>
+      <v-checkbox
+        v-model="checkbox"
+        :error-messages="checkboxErrors"
+        label="Do you agree?"
+        required
+        @change="$v.checkbox.$touch()"
+        @blur="$v.checkbox.$touch()"
+      ></v-checkbox>
+
+      <v-btn class="mr-4" @click="submit"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </form>
+  </v-container>
 </template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email } from "vuelidate/lib/validators";
+
+export default {
+  mixins: [validationMixin],
+
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    email: { required, email },
+    password: {
+      required,
+      // minLength: minLength(8)  // I assume you'd want something like this too
+      containsUppercase: function (value) {
+        return /[A-Z]/.test(value);
+      },
+      containsLowercase: function (value) {
+        return /[a-z]/.test(value);
+      },
+      containsNumber: function (value) {
+        return /[0-9]/.test(value);
+      },
+      containsSpecial: function (value) {
+        return /[#?!@$%^&*-]/.test(value);
+      },
+    },
+    select: { required },
+    checkbox: {
+      checked(val) {
+        return val;
+      },
+    },
+  },
+
+  data: () => ({
+    name: "",
+    email: "",
+    password: "",
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false,
+  }),
+
+  computed: {
+    checkboxErrors() {
+      const errors = [];
+      if (!this.$v.checkbox.$dirty) return errors;
+      !this.$v.checkbox.checked && errors.push("You must agree to continue!");
+      return errors;
+    },
+    selectErrors() {
+      const errors = [];
+      if (!this.$v.select.$dirty) return errors;
+      !this.$v.select.required && errors.push("Item is required");
+      return errors;
+    },
+    nameErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      !this.$v.name.maxLength &&
+        errors.push("Name must be at most 10 characters long");
+      !this.$v.name.required && errors.push("Name is required.");
+      return errors;
+    },
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("Must be valid e-mail");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.required && errors.push("Password is required");
+      !this.$v.password.containsUppercase &&
+        errors.push("Password must contain an uppercase");
+      !this.$v.password.containsLowercase &&
+        errors.push("Password must contain an lowercase");
+      !this.$v.password.containsNumber &&
+        errors.push("Password must contain an number");
+      !this.$v.password.containsSpecial &&
+        errors.push("Password must contain an special");
+      return errors;
+    },
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.email = "";
+      this.password = "";
+      this.select = null;
+      this.checkbox = false;
+    },
+  },
+};
+</script>
